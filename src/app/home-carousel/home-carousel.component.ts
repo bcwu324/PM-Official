56b9aeec09158be9543fcbb449115509f28c1308
unknown <bcwu324@gmail.com>
2021-02-01 20:59:56 +0800

    home carousel done

--- src/app/home-carousel/home-carousel.component.ts
+++ src/app/home-carousel/home-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-carousel',
@@ -8,6 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class HomeCarouselComponent implements OnInit {
 
   constructor() { }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 600,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      // 400: {
+      //   items: 1
+      // },
+      650: {
+        items: 1
+      },
+      760: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      },
+      1400: {
+        items: 1
+      }
+    },
+    nav: false
+  }
 
   ngOnInit(): void {
   }
