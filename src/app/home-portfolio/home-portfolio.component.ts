1c65a8dfcde218186126b71889ebf4aeed2d928e
unknown <bcwu324@gmail.com>
2021-02-02 21:35:48 +0800

    change all image path

--- src/app/home-portfolio/home-portfolio.component.ts
+++ src/app/home-portfolio/home-portfolio.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePortfolioComponent implements OnInit {
   portfolios = [
-    { name: '新生兒寫真', url: '../../assets/images/homePortfolio/新生兒寫真.JPG' },
-    { name: '孕婦寫真', url: '../../assets/images/homePortfolio/孕婦寫真.jpg' },
-    { name: '婚禮紀錄', url: '../../assets/images/homePortfolio/婚禮紀錄.jpg' },
-    { name: '孕婦寫真', url: '../../assets/images/homePortfolio/孕婦寫真.jpg' },
-    { name: '婚紗攝影', url: '../../assets/images/homePortfolio/婚紗攝影.jpg' },
-    { name: '個人藝術照', url: '../../assets/images/homePortfolio/個人藝術照.jpg' },
-    { name: '兒童寫真', url: '../../assets/images/homePortfolio/兒童寫真.jpg' },
-    { name: '全家福', url: '../../assets/images/homePortfolio/全家福.jpg' },
-    { name: '專業形象照', url: '../../assets/images/homePortfolio/專業形象照.jpg' },
+    { name: '新生兒寫真', url: 'assets/images/homePortfolio/新生兒寫真.JPG' },
+    { name: '孕婦寫真', url: 'assets/images/homePortfolio/孕婦寫真.jpg' },
+    { name: '婚禮紀錄', url: 'assets/images/homePortfolio/婚禮紀錄.jpg' },
+    { name: '孕婦寫真', url: 'assets/images/homePortfolio/孕婦寫真.jpg' },
+    { name: '婚紗攝影', url: 'assets/images/homePortfolio/婚紗攝影.jpg' },
+    { name: '個人藝術照', url: 'assets/images/homePortfolio/個人藝術照.jpg' },
+    { name: '兒童寫真', url: 'assets/images/homePortfolio/兒童寫真.jpg' },
+    { name: '全家福', url: 'assets/images/homePortfolio/全家福.jpg' },
+    { name: '專業形象照', url: 'assets/images/homePortfolio/專業形象照.jpg' },
   ]
 
   constructor() { }
