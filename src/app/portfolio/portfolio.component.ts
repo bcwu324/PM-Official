f9ba04350cf5961662df469c875a842af3b750e2
unknown <bcwu324@gmail.com>
2021-03-17 21:55:42 +0800

    embed fb album works

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  constructor() {
 
-  constructor() { }
+  };
+
+  // x(d, s, id) {
+  //   var js;
+  //   if (d.getElementById(id)) {
+  //     return;
+  //   }
+  //   js = d.createElement(s);
+  //   js.id = id;
+  //   js.src = "https://embedsocial.com/embedscript/ei.js";
+  //   d.getElementsByTagName("head")[0].appendChild(js);
+  // }
 
   ngOnInit(): void {
+    // function x(d, s, id) {
+    //   var js;
+    //   if (d.getElementById(id)) {
+    //     return;
+    //   }
+    //   js = d.createElement(s);
+    //   js.id = id;
+    //   js.src = "https://embedsocial.com/embedscript/ei.js";
+    //   d.getElementsByTagName("head")[0].appendChild(js);
+    // }
+    // x(document, "script", "EmbedSocialScript");
   }
 
 }
